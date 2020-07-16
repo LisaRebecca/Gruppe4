9b32de18253f0f496ee4204bbcbd7c17a6f0acc7
LisaRebecca <lisaaschm@gmail.com>
2020-07-16 12:56:22 +0200

    Neue Klasse um DB-Anbindung und SQL-Selects zu überprüfen

--- ButcherCockpitCode/src/DatabaseConnection.java
+++ ButcherCockpitCode/src/DatabaseConnection.java
@@ -5,6 +5,7 @@ import java.sql.SQLException;
 public class DatabaseConnection {
 	public static void main(String[] args) throws SQLException, ClassNotFoundException {
 		Class.forName("com.mysql.jdbc.Driver");
-		Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/bookstore?user=root&password=sequel");
+		Connection connection = DriverManager.getConnection(
+				"jdbc:mysql://localhost:3306/bookstore?user=root&password=sequel");
 	}
 }
