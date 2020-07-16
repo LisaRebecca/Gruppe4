4d0be462c403154012f2d30d58c3a239feb15f62
LisaRebecca <lisaaschm@gmail.com>
2020-07-16 11:58:09 +0200

    Driver muss im Classpath sein Aufbau der Verbindung wirft aktuell keine Exception

---
+++ ButcherCockpitCode/src/DatabaseConnection.java
@@ -0,0 +1,10 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+	public static void main(String[] args) throws SQLException, ClassNotFoundException {
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/bookstore?user=root&password=sequel");
+	}
+}
