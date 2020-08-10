79094a16e602cb28be95ad9280b98b99eb03f085
LisaRebecca <lisaaschm@gmail.com>
2020-08-10 08:34:23 +0200

    manuelles mergen

--- ButcherCockpitCode/src/classes/DatabaseConnector.java
+++ ButcherCockpitCode/src/classes/DatabaseConnector.java
@@ -7,6 +7,21 @@ public class DatabaseConnector extends Observable{
 	
 	public static ResultSet getTableByName(String name) throws SQLException {
 		// hier für die einzelnen Tabellen reinschreiben
-		return conn.createStatement().executeQuery("SELECT * FROM Produkt;");
+		return conn.createStatement().executeQuery("SELECT * FROM "+name+";");
 	}	
+	
+//	public static ResultSet getFieldByName(String tableName, )
+//	
+	public static void updateTable(String tableName, String keyAttrName, String keyAttrVal, String attrName, String attrVal) {
+		try {
+			conn.createStatement().execute(
+					"UPDATE "+tableName+
+					" SET "+attrName+" = "+attrVal+
+					" WHERE "+keyAttrName+" = "+keyAttrVal+";"				
+					);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
