dee6a320c2013a3b6da4861ec4863bd38c9ecc84
julia030100 <62934375+julia030100@users.noreply.github.com>
2020-08-18 12:16:08 +0200

    Update DatabaseConnector.java

--- ButcherCockpitCode/src/classes/DatabaseConnector.java
+++ ButcherCockpitCode/src/classes/DatabaseConnector.java
@@ -6,8 +6,12 @@ public class DatabaseConnector extends Observable{
 	static Connection conn = DatabaseConnection.getDBConnection();
 	
 	public static ResultSet getTableByName(String name) throws SQLException {
-		// hier für die einzelnen Tabellen reinschreiben
-		return conn.createStatement().executeQuery("SELECT * FROM "+name+";");
+		// 1. Alternative: mit einem ActionListener zu arbeiten, 2. mit Switch (Konsoleneingabe) arbeiten
+		return conn.createStatement().executeQuery("SELECT * FROM" +Produkt+";");
+		return conn.createStatement().executeQuery("SELECT * FROM" +Auftragsposten+";");
+		return conn.createStatement().executeQuery("SELECT * FROM" +Bestellungen+";");
+		return conn.createStatement().executeQuery("SELECT * FROM" +Kunden+";");
+		return conn.createStatement().executeQuery("SELECT * FROM" +Mitarbeiter+";");
 	}	
 
 //	public static ResultSet getFieldByName(String tableName, )
