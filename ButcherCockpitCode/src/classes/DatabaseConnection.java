5cef39d95ec20f1da561365f162bd376efa16364
LisaRebecca <lisaaschm@gmail.com>
2020-09-02 15:26:21 +0200

    Kommentieren + Singleton + Exceptions fangen

--- ButcherCockpitCode/src/classes/DatabaseConnection.java
+++ ButcherCockpitCode/src/classes/DatabaseConnection.java
@@ -1,18 +1,33 @@
 package classes;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+/**
+ * Die Klasse {@link DatabaseConnection} stellt genau eine Verbindung zu einer
+ * lokalen MySQL-Datenbank her.
+ * 
+ * @author Lisa Schmidt
+ *
+ */
 public class DatabaseConnection {
+	private static Connection connection = null;
+
+	private DatabaseConnection() {
+	}
+
 	public static Connection getDBConnection() {
-		Connection connection = null;
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
-		} catch (ClassNotFoundException e) {}
-		try {
-			connection = DriverManager.getConnection(
-					"jdbc:mysql://localhost:3306/metzgerei?user=root&password=sequel");
-		} catch (SQLException e) {}
+			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/metzgerei?user=root&password=sequel");
+		} catch (ClassNotFoundException cnf) {
+			cnf.printStackTrace();
+			System.err.println("MySQL-Driver not found.");
+		} catch (SQLException s) {
+			s.printStackTrace();
+			System.err.print("SQL-Exception: Connection to Database could not be established.");
+		}
 		return connection;
 	}
 }
