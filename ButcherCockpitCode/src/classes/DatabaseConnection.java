db13d2052c6e8579ed0eedee3ba7e203b12e3b50
LisaRebecca <lisaaschm@gmail.com>
2020-09-02 15:31:03 +0200

    Doku + Singleton

--- ButcherCockpitCode/src/classes/DatabaseConnection.java
+++ ButcherCockpitCode/src/classes/DatabaseConnection.java
@@ -7,9 +7,6 @@ import java.sql.SQLException;
 /**
  * Die Klasse {@link DatabaseConnection} stellt genau eine Verbindung zu einer
  * lokalen MySQL-Datenbank her.
- * 
- * @author Lisa Schmidt
- *
  */
 public class DatabaseConnection {
 	private static Connection connection = null;
@@ -18,15 +15,18 @@ public class DatabaseConnection {
 	}
 
 	public static Connection getDBConnection() {
-		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/metzgerei?user=root&password=sequel");
-		} catch (ClassNotFoundException cnf) {
-			cnf.printStackTrace();
-			System.err.println("MySQL-Driver not found.");
-		} catch (SQLException s) {
-			s.printStackTrace();
-			System.err.print("SQL-Exception: Connection to Database could not be established.");
+		if (connection == null) {
+			try {
+				Class.forName("com.mysql.jdbc.Driver");
+				connection = DriverManager
+						.getConnection("jdbc:mysql://localhost:3306/metzgerei?user=root&password=sequel");
+			} catch (ClassNotFoundException cnf) {
+				cnf.printStackTrace();
+				System.err.println("MySQL-Driver not found.");
+			} catch (SQLException s) {
+				s.printStackTrace();
+				System.err.print("SQL-Exception: Connection to Database could not be established.");
+			}
 		}
 		return connection;
 	}
