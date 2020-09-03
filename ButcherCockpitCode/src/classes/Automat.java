f83928cd74537ce28160aeadbf5c4bf3f6120e08
Rebecca T <rebecca-tschakste@web.de>
2020-09-03 11:00:49 +0200

    Code aufräumen & Doku

--- ButcherCockpitCode/src/classes/Automat.java
+++ ButcherCockpitCode/src/classes/Automat.java
@@ -12,16 +12,15 @@ import javax.swing.*;
 
 public class Automat extends JFrame {
 
-	Font arial = new Font("Arial", Font.PLAIN, 18);
-	JPanel mainPanel, auswahlPanel, barPanel;
+	Font headerfont = new Font("Arial", Font.BOLD, 20);
+	JPanel mainPanel, selectionPanel, barPanel;
 	JLabel descr_lbl, sum_lbl;
 	JButton buy_btn;
-	ArrayList<Produktauswahl> list_produktauswahl = new ArrayList<Produktauswahl>();
+	ArrayList<Produktauswahl> list_productSelection = new ArrayList<Produktauswahl>();
 
 	public Automat() {
 
 		// Design Initializations
-		this.setTitle("Kühlautomat");
 		try {
 			BufferedImage image = ImageIO.read(new URL(
 					"https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQuzBtedlLeHnfd8uGFz57BYsRIej7Op8mJLA&usqp=CAU"));
@@ -29,34 +28,35 @@ public class Automat extends JFrame {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		
+		this.setTitle("Kühlautomat");
 		this.setVisible(true);
-		this.setSize(500, 400);
-		this.setLocation(800, 100);
+		this.setSize(1200, 400);
+		this.setLocation(200, 100);
 		this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 
 		Container c = getContentPane();
 		mainPanel = new JPanel(new FlowLayout());
 		c.add(mainPanel);
 
-		descr_lbl = new JLabel("Bitte wählen Sie ihre Produkte aus:");
-		descr_lbl.setFont(arial);
+		descr_lbl = new JLabel("Bitte wählen Sie Ihre Produkte aus:");
+		descr_lbl.setFont(headerfont);
+		
+		selectionPanel = new JPanel(new GridLayout(0, 1));
+		selectionPanel.add(descr_lbl);
+		mainPanel.add(selectionPanel);
 
 		String cus_sql = "select name, portionen, haltbar_bis, kilopreis, gewicht_portion from lagerbestand "
 				+ "left join produkte on lagerbestand.produkt = produkte.produkt_id " + "WHERE lagerort='automat1';";
-		JTable jt_verfuegbareProdukte = new JTable(DatabaseConnector.executeDBQuery(cus_sql));
+		JTable jt_obtainableProducts = new JTable(DatabaseConnector.executeDBQuery(cus_sql));
 
-		auswahlPanel = new JPanel(new GridLayout(0, 1));
-		auswahlPanel.add(descr_lbl);
-
-		mainPanel.add(auswahlPanel);
-
-		for (int row = 0; row < jt_verfuegbareProdukte.getRowCount(); row++) {
-			Portion p = new Portion("" + jt_verfuegbareProdukte.getValueAt(row, 0),
-					"" + jt_verfuegbareProdukte.getValueAt(row, 2), "" + jt_verfuegbareProdukte.getValueAt(row, 3),
-					"" + jt_verfuegbareProdukte.getValueAt(row, 4));
-			Produktauswahl produktAuswahl = new Produktauswahl(p);
-			list_produktauswahl.add(produktAuswahl); // Warenkorb
-			auswahlPanel.add(produktAuswahl);
+		for (int row = 0; row < jt_obtainableProducts.getRowCount(); row++) {
+			Portion portion = new Portion("" + jt_obtainableProducts.getValueAt(row, 0),
+					"" + jt_obtainableProducts.getValueAt(row, 2), "" + jt_obtainableProducts.getValueAt(row, 3),
+					"" + jt_obtainableProducts.getValueAt(row, 4));
+			Produktauswahl productSelection = new Produktauswahl(portion);
+			list_productSelection.add(productSelection); // Warenkorb
+			selectionPanel.add(productSelection);
 		}
 
 		barPanel = new JPanel(new GridLayout(3, 1));
@@ -66,13 +66,14 @@ public class Automat extends JFrame {
 		barPanel.add(sum_lbl);
 
 		// Kaufen Button
-		buy_btn = new JButton("Buy");
+		buy_btn = new JButton("Kaufen");
 		buy_btn.setBackground(Color.white);
 		BuyButtonListener bl = new BuyButtonListener();
 		buy_btn.addActionListener(bl);
 		barPanel.add(buy_btn);
-
 		mainPanel.add(barPanel);
+		
+		this.revalidate();
 	}
 
 	public class BuyButtonListener implements ActionListener {
