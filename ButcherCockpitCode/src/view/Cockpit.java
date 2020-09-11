46a756effa25eaacc1cabba9bed4917c33c3281d
Rebecca T <rebecca-tschakste@web.de>
2020-09-11 15:09:28 +0200

    Select Anweisungen

--- ButcherCockpitCode/src/view/Cockpit.java
+++ ButcherCockpitCode/src/view/Cockpit.java
@@ -58,7 +58,8 @@ public class Cockpit extends JFrame {
 		JLabel label_stock = new JLabel("Lagerbestand Gesamt");
 		jp_1.add(label_stock);
 		Tile stock = new Tile(
-				"SELECT name, produkt_id, haltbar_bis, lagerort, portionen, gewicht_portion from lagerbestand "
+				"SELECT name as Produktname, produkt_id as 'Produkt-ID', haltbar_bis as Haltbarkeitsdatum, lagerort as Lagerort, "
+				+ "portionen as Portion, gewicht_portion as Portionsgewicht from lagerbestand "
 						+ "left join produkte on lagerbestand.produkt = produkte.produkt_id;");
 		jp_1.add(stock);
 
@@ -70,7 +71,7 @@ public class Cockpit extends JFrame {
 
 		JLabel label_products = new JLabel("Produktportfolio");
 		jp_2.add(label_products);
-		Tile products = new Tile("SELECT * FROM Produkte;");
+		Tile products = new Tile("SELECT name as Produktname, produkt_id as 'Produkt-ID' , kilopreis as 'Kilopreis [€/kg]', gewicht_portion as Portionsgewicht FROM Produkte;");
 		jp_2.add(products);
 
 		jp_2.add(products);
@@ -83,8 +84,8 @@ public class Cockpit extends JFrame {
 
 		JLabel label_automat = new JLabel("Füllstand Kühlautomat");
 		jp_3.add(label_automat);
-		Tile automat = new Tile("SELECT name, portionen, haltbar_bis, kilopreis, gewicht_portion FROM lagerbestand "
-				+ "LEFT JOIN produkte ON lagerbestand.produkt = produkte.produkt_id WHERE lagerort='automat1';");
+		Tile automat = new Tile("SELECT name as Produktname, portionen as Portion, haltbar_bis as Haltbarkeitsdatum, kilopreis 'Kilopreis [€/kg]', gewicht_portion 'Gewicht [kg/Portion]' FROM lagerbestand "
+						+ "LEFT JOIN produkte ON lagerbestand.produkt = produkte.produkt_id WHERE lagerort='automat1';");
 		jp_3.add(automat);
 		
 		JLabel label_verkaeufe = new JLabel("Verkäufe Kühlautomat");
