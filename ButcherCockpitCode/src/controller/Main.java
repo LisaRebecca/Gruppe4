9d130d47c01db25b1980fc0f012297fd22f5f3c7
LisaRebecca <lisaaschm@gmail.com>
2020-09-11 23:45:45 +0200

    Main-Klasse

---
+++ ButcherCockpitCode/src/controller/Main.java
@@ -0,0 +1,12 @@
+package controller;
+
+import view.Automat;
+import view.Cockpit;
+
+public class Main {
+	public static void main(String[] args) {
+		Automat.main(null);
+		Cockpit.main(null);
+	}
+
+}
