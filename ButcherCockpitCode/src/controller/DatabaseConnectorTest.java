261f1abfa5d022e0d9b37d002111a1a74987d474
LisaRebecca <lisaaschm@gmail.com>
2020-09-12 13:43:55 +0200

    Tests für DBConnector, besseres Error-Handling

--- ButcherCockpitCode/src/controller/DatabaseConnectorTest.java
+++ ButcherCockpitCode/src/controller/DatabaseConnectorTest.java
@@ -1,26 +1,24 @@
 package controller;
 
-import java.awt.*;
-import javax.swing.*;
+import static org.junit.Assert.assertNotNull;
 
-import view.Tile;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.swing.*;
+import org.junit.Test;
 
 @SuppressWarnings("serial")
 public class DatabaseConnectorTest extends JFrame {
-	public DatabaseConnectorTest() {
-		Container c = getContentPane();
-		c.setLayout(new BorderLayout());
-		Tile tile = new Tile(
-				"SELECT name as Produktname, produkt_id as 'Produkt-ID', kilopreis as 'Kilopreis [€/kg]', gewicht_portion as 'Gewicht [kg/Portion]' FROM Produkte;");
-		c.add(tile);
-		this.setVisible(true);
-		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		this.setSize(400, 200);
-		this.setLayout(new FlowLayout());
-		this.revalidate();
+
+	@Test
+	public void executeDBQueryTest() {
+		assertNotNull(DatabaseConnector.executeDBQuery(""));
 	}
 
-	public static void main(String[] args) {
-		new DatabaseConnectorTest();
+	@Test
+	public void buildJTableTest() throws SQLException {
+		ResultSet rs = DatabaseConnection.getDBConnection().createStatement().executeQuery("use metzgerei;");
+		assertNotNull(DatabaseConnector.buildJTable(rs));
 	}
 }
