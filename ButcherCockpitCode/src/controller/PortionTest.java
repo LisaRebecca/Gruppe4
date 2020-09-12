7140ca7f23088d45a34c42067a32028870e77cc7
a-sch <a-sch@DESKTOP-SCNK163.Speedport_W_724V_09011603_06_006>
2020-09-12 15:42:21 +0200

    PortionTest fertig

---
+++ ButcherCockpitCode/src/controller/PortionTest.java
@@ -0,0 +1,63 @@
+package controller;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+public class PortionTest {
+	
+	@Test
+	public void testName () {
+		Portion portion = new Portion("Schnitzel", "5","16.09.2020", "16.90", "0.3");
+		Assert.assertEquals("Schnitzel", portion.getName());
+	}
+	@Test
+	public void testLagermenge () {
+	Portion portion = new Portion("Schnitzel", "5","16.09.2020", "16.90", "0.3");
+	Assert.assertEquals(5, portion.getLagermenge());
+	}
+	@Test
+	public void testHaltbarkeit () {
+	Portion portion = new Portion("Schnitzel", "5","16.09.2020", "16.90", "0.3");
+	Assert.assertEquals("16.09.2020", portion.getHaltbar());
+	}
+	
+	@Test
+	public void testKilopreis () {
+	Portion portion = new Portion("Schnitzel", "5","16.09.2020", "16.90", "0.3");
+	Assert.assertEquals(16.90, portion.getKilopreis());
+	}
+	
+	@Test
+	public void testPortionsgewicht () {
+	Portion portion = new Portion("Schnitzel", "5","16.09.2020", "16.90", "0.3");
+	Assert.assertEquals(0.3, portion.getPortionsgewichtKG());
+	}
+	@Test
+	public void testPortionspreis () {
+	Portion portion = new Portion("Schnitzel", "5","16.09.2020", "16.90", "0.3");
+	Assert.assertEquals(Double.parseDouble("16.90") * Double.parseDouble("0.3"), portion.getPortionspreis());
+	}
+	
+	@Test(expected=NumberFormatException.class)
+	public void testWrongKilopreisThrowsException() {
+		Portion portion = new Portion("Schnitzel", "5","16.09.2020", "abc", "0.3");
+	}
+	@Test(expected=NumberFormatException.class)
+	public void testWrongPortionsgewichtThrowsException() {
+		Portion portion = new Portion("Schnitzel", "5","16.09.2020", "16.90", "ab");
+	}
+	@Test(expected=NumberFormatException.class)
+	public void testWrongPortionspreisThrowsException() {
+		Portion portion = new Portion("Schnitzel", "5","16.09.2020", "abc", "0.3");
+	}
+	@Test(expected=NumberFormatException.class)
+	public void testWrongLagermengeThrowsException() {
+		Portion portion = new Portion("Schnitzel", "?.)?","16.09.2020", "abc", "0.3");
+	}
+	@Test
+	public void testgetPortionsgewichtGramm() {
+		Portion portion = new Portion("Schnitzel", "5","16.09.2020", "16.90", "0.3");
+		Assert.assertEquals(300, portion.getPortionsgewichtGramm());
+	}
+}
