4605baee1e437d9bb2102b595174973b2552f587
LisaRebecca <lisaaschm@gmail.com>
2020-09-12 17:02:16 +0200

    JUnit Test von MyTools

--- ButcherCockpitCode/src/Tools/MyTools.java
+++ ButcherCockpitCode/src/Tools/MyTools.java
@@ -2,7 +2,7 @@ package Tools;
 import java.text.NumberFormat;
 
 public class MyTools {
-	public static final NumberFormat currencyFormatter = NumberFormat.getInstance(); //.setMaximumFractionDigits(2);
+	public static final NumberFormat currencyFormatter = NumberFormat.getInstance();
 	public static String formatAsCurrency(double d) {
 		currencyFormatter.setMaximumFractionDigits(2);
 		currencyFormatter.setMinimumFractionDigits(2);
