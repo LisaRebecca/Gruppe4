eeedb4a84a66534f4aa53c96237f1315bb615baa
LisaRebecca <lisaaschm@gmail.com>
2020-09-13 16:56:21 +0200

    Test erweitert

--- ButcherCockpitCode/src/controller/AutomatTest.java
+++ ButcherCockpitCode/src/controller/AutomatTest.java
@@ -13,7 +13,7 @@ import view.Automat;
  *
  */
 public class AutomatTest {
-	Object[][] data = {{"Lendchen", "5", "2021-12-31", "15.00", "0.5"}};
+	Object[][] data = {{"Lendchen", "5", "2021-12-31", "15.00", "0.5"}, {"Rippchen", "3", "2021-09-05", "8.00", "0.25"}};
 	Object[] colNames = {"","","","",""};
 	
 	Automat automat = new Automat(new JTable(data, colNames));
@@ -21,7 +21,7 @@ public class AutomatTest {
 	@Test
 	public void testBerechneGesamtpreis() {
 		automat.list_productSelection.get(0).getJb_more().doClick();
-		Assert.assertEquals(automat.getGesamtpreis(), 7.50, 1);
-		
+		automat.list_productSelection.get(1).getJb_more().doClick();
+		Assert.assertEquals(automat.getGesamtpreis(), 9.50, 0.1);
 	}
 }
