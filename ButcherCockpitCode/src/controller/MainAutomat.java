6a7483b7b2a0c19ffdbc7bbf99b709922defd6bf
LisaRebecca <lisaaschm@gmail.com>
2020-09-28 19:20:56 +0200

    überflüssige MainAutomat gelöscht, alles nur noch aus Main

--- ButcherCockpitCode/src/controller/MainAutomat.java
+++
@@ -1,9 +0,0 @@
-package controller;
-
-public class MainAutomat {
-	public static void main(String[] args) {
-		
-		LoginController.set(new LoginControllerAutomat());
-		Database.set(new RealDatabase());
-	}
-}
