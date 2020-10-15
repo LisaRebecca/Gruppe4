3ee754cda36697807a81b1dde61a44114a57d9ee
7Rebecca <62660708+7Rebecca@users.noreply.github.com>
2020-10-15 10:48:32 +0200

    Payment in Maven Struktur

---
+++ Maven/ButcherCockpitMaven/src/main/java/Tools/GiftCardException.java
@@ -0,0 +1,11 @@
+package Tools;
+
+public class GiftCardException extends Exception{
+	GiftCardException() {
+		super("Die Zahlung mit Ihrer Gutscheinkarte konnte nicht abgeschlossen werden." 
+				+ "Häufigste Ursache: Nicht genügend Guthaben"
+				+ "Bitte fragen Sie bei Gelegenheit einen verfügbaren Mitarbeiter oder wählen Sie eine andere Zahlungsmethode." 
+				+ "Wir entschuldigen uns für die Unannehmlichkeit.");
+	}
+
+}
