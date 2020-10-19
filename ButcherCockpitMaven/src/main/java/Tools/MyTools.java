942923c4bdd3125edf7c202a3e3480f3919ad40f
LisaRebecca <lisaaschm@gmail.com>
2020-10-19 10:30:20 +0200

    Konvertierung Resultset -> Jtable in MyTools

--- ButcherCockpitMaven/src/main/java/Tools/MyTools.java
+++ ButcherCockpitMaven/src/main/java/Tools/MyTools.java
@@ -1,12 +1,22 @@
 package Tools;
 
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
 import java.text.NumberFormat;
+import java.util.Vector;
+
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+import errorhandling.ButcherException;
 
 public class MyTools {
 	public static final NumberFormat currencyFormatter = NumberFormat.getInstance();
 
 	/**
 	 * Formatiert Zahlen in eine Währung
+	 * 
 	 * @param d Wert, der in Währung umgewandelt werden soll
 	 * @return den Eingabewert formatiert als Währung
 	 */
@@ -14,5 +24,35 @@ public class MyTools {
 		currencyFormatter.setMaximumFractionDigits(2);
 		currencyFormatter.setMinimumFractionDigits(2);
 		return currencyFormatter.format(d) + "€";
+	}
+
+	public static JTable resultSetToTable(ResultSet result) throws ButcherException {
+		Vector<Vector<String>> rows;
+		Vector<String> columnLabels;
+
+		ResultSetMetaData metaData;
+		try {
+			metaData = result.getMetaData();
+
+			int columnCount = metaData.getColumnCount();
+			columnLabels = new Vector<String>();
+
+			for (int columnIndex = 1; columnIndex <= columnCount; columnIndex++) {
+				columnLabels.add(metaData.getColumnLabel(columnIndex));
+			}
+
+			rows = new Vector<Vector<String>>();
+			Vector<String> singleRow;
+			while (result.next()) {
+				singleRow = new Vector<String>();
+				for (int columnIndex = 1; columnIndex <= columnCount; columnIndex++) {
+					singleRow.add("" + result.getObject(columnIndex));
+				}
+				rows.add(singleRow);
+			}
+			return new JTable(new DefaultTableModel(rows, columnLabels));
+		} catch (SQLException e) {
+			throw new ButcherException(e, "Datenbankfehler", "Bitte wenden Sie sich an einen Mitarbeiter");
+		}
 	}
 }
