297d626e5fc29fe9b6910b324c541d618ea538ac
LisaRebecca <lisaaschm@gmail.com>
2020-10-23 11:20:33 +0200

    first (!) Nullpointer im LoginController Filled resolved

--- ButcherCockpitMaven/src/main/java/controller/Main.java
+++ ButcherCockpitMaven/src/main/java/controller/Main.java
@@ -1,9 +1,10 @@
 package controller;
 
 import controller.login.LoginController;
-
+import controller.login.LoginControllerFilled;
 import controller.login.LoginControllerStandard;
 import data.Database;
+import data.MockDatabase;
 import data.RealDatabase;
 import errorhandling.AbstractButcherException;
 import errorhandling.ExceptionHandler;
@@ -13,13 +14,14 @@ import payment.Payment;
 import view.CockpitFactory;
 import view.Factory;
 
+@SuppressWarnings("unused") // wir kommentierern hier manche sachen abwechselnd aus, somit ist immer etwas 'unused'
 public class Main {
 	public static void main(String[] args) {
- 		LoginController.set(new LoginControllerStandard());
-// 		LoginController.set(new LoginControllerFilled());
+// 		LoginController.set(new LoginControllerStandard());
+ 		LoginController.set(new LoginControllerFilled());
  		
- 		Database.set(new RealDatabase());
-// 		Database.set(new MockDatabase());
+// 		Database.set(new RealDatabase());
+ 		Database.set(new MockDatabase());
  		
  		ExceptionHandler.set(new ExceptionHandlerDebug());
 //		ExceptionHandler.set(new ExceptionHandlerUser());
@@ -27,13 +29,14 @@ public class Main {
  		Payment.set(new GiftCard());
 // 		Payment.set(new CreditCard());
  		
+ 		Factory.set(new CockpitFactory());
+// 		Factory.set(new AutomatFactory());
+
  		try {
  			LoginController.get().giveControl();
  		} catch (AbstractButcherException e) {
  			ExceptionHandler.get().showException(e);
  
  		}
- 		Factory.set(new CockpitFactory());
-// 		Factory.set(new AutomatFactory());
 	}
 }
