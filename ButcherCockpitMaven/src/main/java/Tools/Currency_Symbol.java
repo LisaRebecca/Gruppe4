d350424cd181e554f0cffb691b8f9a2fc32f15f4
MarcelSielaff <marcel.sielaff@gmx.net>
2020-10-23 13:54:02 +0200

    Currency_Symbol Test Klasse

--- ButcherCockpitMaven/src/main/java/Tools/Currency_Symbol.java
+++ ButcherCockpitMaven/src/main/java/Tools/Currency_Symbol.java
@@ -1,7 +1,11 @@
 package Tools;
 
 public class Currency_Symbol {
-	private static String currency_symbol = "$";
+	private static String currency_symbol="€";
+
+	public static String setCurrency_Symbol(String currency_symbol_new) {
+		return currency_symbol = currency_symbol_new;
+	}
 
 	public static String getCurrency_Symbol() {
 
