687b15408bbe4f6f8920dd1912e476684d30cdc1
LisaRebecca <lisaaschm@gmail.com>
2020-10-27 18:43:31 +0100

    sync credit card + gift card changes

--- ButcherCockpitMaven/src/main/java/payment/CreditCardPayment.java
+++ ButcherCockpitMaven/src/main/java/payment/CreditCardPayment.java
@@ -2,19 +2,19 @@ package payment;
 
 import java.util.ResourceBundle;
 
-
 import errorhandling.CreditCardException;
+import errorhandling.SQLButcherException;
 import payment.Payment;
 
-public class CreditCardPayment extends Payment{
+public class CreditCardPayment extends Payment {
 	private final ResourceBundle language;
-	
+
 	public CreditCardPayment() {
 		this.language = ResourceBundle.getBundle("i18n/creditcard/creditcard_en");
 	}
-	
-	public String processPurchase() throws CreditCardException{
+
+	public String processPurchase(double gesamtpreis) throws SQLButcherException {
+		saveIntoDatabase(gesamtpreis);
 		return this.language.getString("processing_message");
-		
 	}
 }
--- ButcherCockpitMaven/src/main/java/payment/GiftCardPayment.java
+++ ButcherCockpitMaven/src/main/java/payment/GiftCardPayment.java
@@ -2,21 +2,20 @@ package payment;
 
 import java.util.ResourceBundle;
 
-
 import errorhandling.GiftCardException;
+import errorhandling.SQLButcherException;
 import payment.Payment;
 
-public class GiftCardPayment extends Payment{
-	
+public class GiftCardPayment extends Payment {
+
 	private final ResourceBundle language;
-	
-	public GiftCardPayment () {
+
+	public GiftCardPayment() {
 		this.language = ResourceBundle.getBundle("i18n/giftcard/giftcard_en");
 	}
-	
-	public String processPurchase() throws GiftCardException{
+
+	public String processPurchase(double gesamtpreis) throws SQLButcherException {
+		saveIntoDatabase(gesamtpreis);
 		return this.language.getString("processing_message");
-		
 	}
-	
 }
