cd27c69264df10a0eeb39a515852cc6073cfd6a2
LisaRebecca <lisaaschm@gmail.com>
2020-10-27 19:58:43 +0100

    Formatter Test aktualisieren

--- ButcherCockpitMaven/src/test/java/tools/MyToolsTest.java
+++ ButcherCockpitMaven/src/test/java/tools/MyToolsTest.java
@@ -3,21 +3,19 @@ package tools;
 import org.junit.Assert;
 import org.junit.Test;
 
-import Tools.MyTools;
-
 public class MyToolsTest {
 	@Test
 	public void testFormatAsCurrency1() {
-	Assert.assertEquals("350,00€", MyTools.formatAsCurrency(350.0000));
+	Assert.assertEquals("350,00€", Tools.Formatter.formatAsCurrency(350.0000));
 	}
 	
 	@Test
 	public void testFormatAsCurrency2() {
-	Assert.assertEquals("175,00€", MyTools.formatAsCurrency(175));
+	Assert.assertEquals("175,00€", Tools.Formatter.formatAsCurrency(175));
 	}
 	
 	@Test
 	public void testFormatAsCurrency3() {
-	Assert.assertEquals("89,90€", MyTools.formatAsCurrency(89.9));
+	Assert.assertEquals("89,90€", Tools.Formatter.formatAsCurrency(89.9));
 	}
 }
