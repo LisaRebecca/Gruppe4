df9535dcf2b806d262d2d96bda7731f29ef0544d
a-sch <a-sch@DESKTOP-SCNK163.speedport.ip>
2020-10-28 11:34:24 +0100

    Factories werfen Exceptions

--- ButcherCockpitMaven/src/main/java/view/Factory.java
+++ ButcherCockpitMaven/src/main/java/view/Factory.java
@@ -1,15 +1,21 @@
 package view;
 
 import errorhandling.AbstractButcherException;
+import errorhandling.ButcherException;
 
 public abstract class Factory {
 
 	
 	private static Factory factory;
 	
-	public static Factory get() {
-		return factory;
-	}
+	public static Factory get() throws ButcherException {
+		try {
+			return factory;}
+		catch(NullPointerException e) {
+			throw new ButcherException(e, "technische Probleme", "Bitte an IT-Support wenden"); }
+		
+		}
+	
 	
 	public static void set(Factory factory) {
 		Factory.factory = factory;
