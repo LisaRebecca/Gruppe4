7fd607c3c68080990ca0958637de6690402d57fc
LisaRebecca <lisaaschm@gmail.com>
2020-10-30 13:18:14 +0100

    Exceptions payment korrigieren

--- ButcherCockpitMaven/src/main/java/errorhandling/GiftCardException.java
+++ ButcherCockpitMaven/src/main/java/errorhandling/GiftCardException.java
@@ -2,25 +2,15 @@ package errorhandling;
 
 import java.util.ResourceBundle;
 
-<<<<<<< Upstream, based on origin/master
 /**
- *  GiftCardException dient der Fehlerbehandlung, im Falle von Schwierigkeiten beim Einlesen der Gutscheinkarte und dem damit einhergehenden Zahlungsvorgang.
- *	Sie soll dem Kunden eine Beschreibung der (möglichen) Fehlerursachen geben, sowie Änsatze, wie dieser weiter vorgehen kann. 
+ * GiftCardException dient der Fehlerbehandlung, im Falle von Schwierigkeiten
+ * beim Einlesen der Gutscheinkarte und dem damit einhergehenden
+ * Zahlungsvorgang. Sie soll dem Kunden eine Beschreibung der (möglichen)
+ * Fehlerursachen geben, sowie Änsatze, wie dieser weiter vorgehen kann.
  */
-
-public class GiftCardException extends AbstractButcherException{
-	public GiftCardException(AbstractButcherException e) {
-		super(e, ResourceBundle.getBundle("i18n/giftcard_exception/giftcard_exception_en").getString("title"), 
-=======
 public class GiftCardException extends PaymentButcherException {
 	public GiftCardException() {
 		super(null, ResourceBundle.getBundle("i18n/giftcard_exception/giftcard_exception_en").getString("title"),
->>>>>>> 41aac9b Exceptions im Payment abgestimmt
 				ResourceBundle.getBundle("i18n/giftcard_exception/giftcard_exception_en").getString("error_text"));
-<<<<<<< Upstream, based on origin/master
-		
-=======
->>>>>>> 41aac9b Exceptions im Payment abgestimmt
 	}
-
 }
