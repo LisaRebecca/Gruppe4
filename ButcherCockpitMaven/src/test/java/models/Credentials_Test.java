19e972973f07f5ed8a8db936f29badf986650bb0
MarcelSielaff <marcel.sielaff@gmx.net>
2020-11-01 16:50:11 +0100

    Credentials Test erweitert

--- ButcherCockpitMaven/src/test/java/models/Credentials_Test.java
+++ ButcherCockpitMaven/src/test/java/models/Credentials_Test.java
@@ -6,14 +6,27 @@ import junit.framework.Assert;
 
 public class Credentials_Test {
 
-	
-	
-
 	@Test
-	public void getIsSetTest() {
+	public void getIsSetTest1() {
 		Credentials.setPassword("testpassword");
 		Credentials.setUsername("testusername");
 		Assert.assertEquals(true, Credentials.getIsSet());
+
+	}
+
+	@Test
+	public void getIsSetTest2() {
+		Credentials.setUsername(null);
+		Credentials.setPassword("test");
+		Assert.assertEquals(false, Credentials.getIsSet());
+
+	}
+
+	@Test
+	public void getIsSetTest3() {
+		Credentials.setPassword(null);
+		Credentials.setUsername(null);
+		Assert.assertEquals(false, Credentials.getIsSet());
 
 	}
 
