1eee5debacc6630b6efbbf983fceb2bdad7078e2
LisaRebecca <lisaaschm@gmail.com>
2020-11-02 20:11:59 +0100

    ExceptionHandler

--- ButcherCockpitMaven/src/main/java/errorhandling/ExceptionHandler.java
+++ ButcherCockpitMaven/src/main/java/errorhandling/ExceptionHandler.java
@@ -1,17 +1,20 @@
 package errorhandling;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.logging.FileHandler;
 import java.util.logging.Handler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
+
 /**
- * Der ExceptionHandler dient als Singleton dazu, festzulegen ob der ExceptionHandlerUser
- * oder - Debug ausgewählt ist. Je nachdem welcher Handler gewählt ist, wird anders
- * mit den auftretenden Exceptions verfahren. Das unterschiedliche Verhalten wird
- * im jeweiligen Handler implementiert. Außerdem wird im Konstruktor ein Log-File angelegt,
- * in dem die geworfenen Exceptions dokumentiert werden.
+ * Der ExceptionHandler dient als Singleton dazu, festzulegen ob der
+ * ExceptionHandlerUser oder - Debug ausgewählt ist. Je nachdem welcher Handler
+ * gewählt ist, wird anders mit den auftretenden Exceptions verfahren. Das
+ * unterschiedliche Verhalten wird im jeweiligen Handler implementiert. Außerdem
+ * wird im Konstruktor ein Log-File angelegt, in dem die geworfenen Exceptions
+ * dokumentiert werden.
  * 
  * @author a-sch
  *
@@ -21,17 +24,21 @@ public abstract class ExceptionHandler {
 	private static ExceptionHandler exceptionHandler = new ExceptionHandlerUser();
 	private static Logger logger;
 	private static Handler handler;
+	private static File file;
 
 	public ExceptionHandler() {
 		logger = Logger.getLogger(ExceptionHandlerUser.class.getName());
+		file = new File("logFile.log");
 		try {
-			handler = new FileHandler("logs/exception_logging.log");
+			if (!file.exists())
+				file.createNewFile();
+			handler = new FileHandler(file.getAbsolutePath());
 		} catch (SecurityException e) {
-			exceptionHandler.showException(new ButcherException(e, "Sicherheitsfehler beim Logging",
-					"Bitte wenden Sie sich an den IT-Support"));
+			logException(e);
+			System.out.println(e);
 		} catch (IOException e) {
-			exceptionHandler.showException(new ButcherException(e, "Log-File Zugriff fehlgeschlagen",
-					"Bitte wenden Sie sich an den IT-Support"));
+			logException(e);
+			System.out.println(e);
 		}
 		logger.setUseParentHandlers(false); // only print to file not to console
 		handler.setFormatter(new SimpleFormatter());
@@ -41,25 +48,29 @@ public abstract class ExceptionHandler {
 	public static ExceptionHandler get() {
 		return exceptionHandler;
 	}
+
 	/**
-	 * 
+	 * Setter
 	 * @param exceptionHandler
 	 */
 	public static void set(ExceptionHandler exceptionHandler) {
 		ExceptionHandler.exceptionHandler = exceptionHandler;
 	}
+
 	/**
 	 * Methode zum Speichern der auftretenden Exceptions in der Log-File.
+	 * 
 	 * @param Exception e
 	 */
 	protected void logException(Exception e) {
 		logger.log(Level.INFO, "Exception geworfen", e);
 	}
+
 	/**
-	 * abstrakte Methode die im ExceptionHandlerUser bzw. - Debug überschrieben 
-	 * wird um die geworfene Exception auf eine bestimmte Weise anzuzeigen.
+	 * abstrakte Methode die im ExceptionHandlerUser bzw. - Debug überschrieben wird
+	 * um die geworfene Exception auf eine bestimmte Weise anzuzeigen.
 	 * 
-	 * @param AbstractButcherException e 
+	 * @param AbstractButcherException e
 	 */
 	public abstract void showException(AbstractButcherException e);
 
